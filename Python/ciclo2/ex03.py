d6626f8b9a338cb8e47967bb83c8464f84a6131d
bbeggiato <bbeggiato@gmail.com>
2022-10-29 16:00:34 -0300

    Update ex03.py

--- Python/ciclo2/ex03.py
+++ Python/ciclo2/ex03.py
@@ -1,11 +1,13 @@
-
+# Feito em Python 3.10.8
 while True:
     lista = []
-
+    print('--' * 28)
+    print('Qual o menor valor entre os 3 Números Digitados Abaixo?')
+    print('--'* 28)
     try:
-        Valor1 = int(input('Digite o Primeiro valor:'))
-        Valor2 = int(input('Digite o Segundo valor:'))
-        Valor3 = int(input('Digite o Terceiro valor:'))
+        Valor1 = int(input('Digite o Primeiro valor: '))
+        Valor2 = int(input('Digite o Segundo valor:  '))
+        Valor3 = int(input('Digite o Terceiro valor: '))
 
     except ValueError:
         print('Favor digitar apenas valores numéricos')
@@ -22,6 +24,11 @@ menor = min(lista)
 print('--'*18)
 print(f'\033[36mO Menor Valor entre os três é:\033[m \033[33m {menor}\033[m')
 print('--'*18)
+
+
+
+
+
 
 
 
