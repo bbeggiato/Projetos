2261ffe3707432437e4b23d0f0a0df0357f0f7c8
bbeggiato <bbeggiato@gmail.com>
2022-10-29 17:32:49 -0300

    corrige operadores
    ao invés de >, correto é >=

--- Python/ciclo2/ex02.py
+++ Python/ciclo2/ex02.py
@@ -1,6 +1,10 @@
 #Python 3.10.8
-import math
+from math import sqrt
+from time import sleep
 while True:
+    print('--'*26)
+    print('Calcule a área caso os valores formem um Triângulo:  ')
+    print('--'*26)
     try:
         a = int(input('Digite o valor do lado "A": '))
         b = int(input('Digite o valor do lado "B": '))
@@ -13,12 +17,15 @@ while True:
             break
     except ValueError:
         print('Você não digitou um número!')
+print()
 
-if a > (b+c) or b > (a+c) or c > (a+b):
+if a >= (b+c) or b >= (a+c) or c >= (a+b):
     print(f'Os valores de a: {a}, b: {b} e c: {c} não formam um Triângulo')
 else:
     p = (a+b+c)/2 # Calculo do Perimetro
-    area = math.sqrt(p*(p-a)*(p-b)*(p-c)) # Com o Perímetro calculo a área
+    area = sqrt(p*(p-a)*(p-b)*(p-c)) # Com o Perímetro calculo a área
+    print('Calculando a Área do Triângulo....')
+    sleep(2.5)
     print('--' * 14)
     print(f'A \033[36mÁrea\033[m do triângulo é:\033[33m {area:.2f}\033[m')
     print('--' * 14)
